43e0b26f7e5c2780452b90261ef66cebd71603c6
Yasmin Cardoso Magagnin <yasminhayashi234@gmail.com>
2025-09-01 16:47:20 -0300

    Update start_tudo.bat

--- start_tudo.bat
+++ start_tudo.bat
@@ -2,8 +2,11 @@
 cd /d "%~dp0"
 
 REM Compila os programas (descomente se quiser recompilar sempre)
-REM g++ back\pipes\processo1.cpp -o back\pipes\processo1.exe
-REM g++ back\pipes\processo2.cpp -o back\pipes\processo2.exe
+g++ back\pipes\processo1.cpp -o back\pipes\processo1.exe
+g++ back\pipes\processo2.cpp -o back\pipes\processo2.exe
 
 REM Inicia o servidor Node.js
 start cmd /k "node server.js"
+
+REM Abre o navegador na página inicial
+start "" http://localhost:3000/html/index.html
